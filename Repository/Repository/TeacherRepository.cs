7cce0bbc052a2d03324db25c62335dc856616cdc
minhtdev <aigiabeomap@gmail.com>
2023-10-23 14:47:33 +0700

    update

--- Repository/Repository/TeacherRepository.cs
+++ Repository/Repository/TeacherRepository.cs
@@ -1,8 +1,45 @@
-﻿using Repository.IRepository;
+﻿using AutoMapper;
+using BusinessObject.Models;
+using DataAccess.DAO;
+using DTO.Request.Teacher;
+using DTO.Response.Teacher;
+using Repository.IRepository;
 
 namespace Repository.Repository
 {
     public class TeacherRepository : ITeacherRepository
     {
+        TeacherDAO teacherDAO;
+        IMapper _mapper;
+
+        public TeacherRepository(FAPDbContext context, IMapper mapper)
+        {
+            teacherDAO = new TeacherDAO(context);
+            _mapper = mapper;
+        }
+
+        public bool AddTeacher(TeacherAddDTO teacher)
+        {
+            return teacherDAO.AddTeacher(_mapper.Map<Teacher>(teacher));
+        }
+
+        public bool DeleteTeacher(TeacherUpdateDTO teacher)
+        {
+            return teacherDAO.DeleteTeacher(_mapper.Map<Teacher>(teacher));
+        }
+
+        public TeacherResponseDTO? GetTeacherById(int id)
+        {
+            return _mapper.Map<TeacherResponseDTO>(teacherDAO.GetTeacherById(id));
+        }
+        public List<TeacherResponseDTO> GetTeachers()
+        {
+            return _mapper.Map<List<TeacherResponseDTO>>(teacherDAO.GetTeachers());
+        }
+
+        public bool UpdateTeacher(TeacherUpdateDTO teacher)
+        {
+            return teacherDAO.UpdateTeacher(_mapper.Map<Teacher>(teacher));
+        }
     }
 }
